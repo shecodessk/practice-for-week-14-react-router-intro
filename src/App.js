3f805faf9f8d4a94f752c3d85ec04db2c94b3b39
Stephany K <stephanykleen@gmail.com>
2023-09-23 20:47:38 +0000

    Adding React Router to components paths

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
-
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route>
+          <h1>Page Not Found</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
